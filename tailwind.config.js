7344ebc4dabf0670930d39313b4117178a39b3b3
Khushiyant <khushiyant2002@gmail.com>
2023-05-30 01:28:18 +0530

    chore(add): tailwind--ts,svelte

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.ts",
+    "./src/**/*.svelte",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
