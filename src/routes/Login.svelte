832b3bad2575e3549670256c1dbc551decb571ea
Khushiyant <khushiyant2002@gmail.com>
2023-05-29 21:11:15 +0530

    chore(add): login--skeleton

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,1 +1,34 @@
-<h1>Login</h1>
+<script lang="ts">
+  import "@appwrite.io/pink";
+  import "@appwrite.io/pink-icons";
+</script>
+
+<!-- Markup Area Begin -->
+<div class="container u-full-screen-height" style="border-radius: var(--border-radius-small);">
+    <form class="form u-width-full-line u-max-width-500">
+        <ul class="form-list">
+          <li class="form-item">
+            <label class="label u-color-text-gray">Name</label>
+            <div class="input-text-wrapper">
+              <input type="text" class="input-text" placeholder="Name" />
+            </div>
+          </li>
+          <li class="form-item">
+            <label class="label u-color-text-gray">Email</label>
+            <div class="input-text-wrapper">
+              <input type="text" class="input-text" placeholder="Email" />
+            </div>
+          </li>
+        </ul>
+      
+        <button class="button u-margin-32 ">
+          <span class="text">Submit</span>
+        </button>
+      </form>
+</div>
+
+<!-- Markup Area End -->
+
+<style>
+
+</style>
