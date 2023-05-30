757973dffca73f86e473dc0ca69c1474515e8cac
Kranium2002 <vidhumathur2002@gmail.com>
2023-05-30 15:25:14 +0530

    chore(add): component--nav colorScheme
    Setup Tailwind css according to naming conventions and added color scheme and navbar component

---
+++ src/components/ProfileCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let name: string = "Felix";
+    export let email: string =  "f@gmail.com";
+    export let university: string = "GGSIPU";
+    export let avatarUrl: string = "https://api.dicebear.com/6.x/adventurer/svg?seed=Felix";
+  </script>
+
+  <div class="bg-neutral-200 rounded-lg p-4 w-1/2 items-center shadow-neutral-150 shadow-lg text-center">
+    <img class="w-24 h-24 rounded-full mx-auto mb-4" src={avatarUrl} alt="Profile Picture">
+    <h2 class="text-xl text-primary-100 font-semibold mx-auto mb-2 bg-neutral-150 w-1/2 rounded-lg py-1">{name}</h2>
+    <p class="text-primary-100 mb-2 bg-neutral-150 w-1/2 rounded-lg mx-auto py-1">{email}</p>
+    <p class="text-primary-100 bg-neutral-150 w-1/2 rounded-lg mx-auto py-1">{university}</p>
+  </div>
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.ts",
     "./src/**/*.svelte",
     "./**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        teansparent: "transparent",
+        primary:{
+          100: "#f02c64",
+          200: "#da1b5a",
+          300: "#bf0d51",
+        },
+        neutral:{
+          0: "#fffefe",
+          5: "#fafbfe",
+          10: "#f2f2f8",
+          30: "#e8ebf1",
+          50: "#c5c6d9",
+          70: "#848ca3",
+          100: "#606b7a",
+          120: "#4f5769",
+          150: "#373a4f",
+          200: "#27293a",
+          300: "#1b1b27",
+          400: "#161723",
+          500: "#14141f",
+        },}
+    },
   },
   plugins: [],
 }
