a342f19f8b8ee8427ae3db69f2ee3a148504ae49
Khushiyant <khushiyant2002@gmail.com>
2023-06-11 01:38:59 +0530

    chore(add): function--appwrite

--- functions/create-chatroom/src/index.py
+++ functions/create-chatroom/src/index.py
@@ -5,7 +5,6 @@ from appwrite.services.databases import Databases
 from appwrite.permission import Permission
 from appwrite.id import ID
 from appwrite.role import Role
-import time
 
 """
   'req' variable has:
@@ -46,38 +45,41 @@ def main(req, res):
   
  
   dbId = req.variables.get('APPWRITE_FUNCTION_DATABASE_ID', None)
-    
-  decoded_data = json.loads(req.payload)
-  user_Ids = []
-  user_Ids = decoded_data['user_ids']
-  print(decoded_data)
+# {"message":"Hello from the other side","payload":"{\nuser_ids: [\"1234\", \"234\"]\n}"}
   
-  # # TODO: Check if This is needed?
-  # session_id = req.payload['session-id']
-  dateTime = datetime.datetime.now()
-  dateTime =  dateTime.microsecond
-  
-  permissions = []
-  for user in user_Ids:
-    permissions.append(Permission.write(Role(user)))
-    permissions.append(Permission.read(Role(user)))
+ 
 
 
   # https://appwrite.io/docs/server/functions?sdk=python-default
   # https://github.com/appwrite/sdk-for-python/blob/master/appwrite/permission.py
-  try:
-    collection = database.create_collection(dbId, ID.unique(),  'Chat_Room_' + str(dateTime), permissions) 
-    print("Collection created")
+  decoded_data = json.loads(str(req.payload))
 
-    database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='msg', label='msg', required=False, array=False, default='', options=[],size=3000)
-    database.create_datetime_attribute(database_id=dbId, collection_id=collection['$id'], key='time', label='time', required=True, array=False, default='', options=[])
-    database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='sender-name', label='sender-name', required=True, array=False, default='', options=[],size=255)
-    database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='img-id', label='img-id', required=False, array=False, default='', options=[],size=50)
-    database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='sender-id', label='sender-id', required=True, array=False, default='', options=[],size=50)
-    database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='session-id', label='session-id', required=True, array=False, default='', options=[],size=50)
-    print("Attributes created")
-    # Wait for 2 seconds to finishing creating the attributes
-    # time.sleep(2)
+  try:
+
+    user_Ids = []
+    user_Ids = decoded_data['user_ids']
+    print(decoded_data)
+    
+    # # TODO: Check if This is needed?
+    # session_id = req.payload['session-id']
+    dateTime = datetime.datetime.now()
+    dateTime =  dateTime.microsecond
+    
+    permissions = []
+    for user in user_Ids:
+      permissions.append(Permission.write(Role.user(user)))
+      permissions.append(Permission.read(Role.user(user)))
+      collection = database.create_collection(dbId, ID.unique(),  'Chat_Room_' + str(dateTime), permissions) 
+      print("Collection created")
+
+      database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='msg', required=False, array=False, default='',size=3000)
+      database.create_datetime_attribute(database_id=dbId, collection_id=collection['$id'], key='time', required=True, array=False, default='')
+      database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='sender-name', required=True, array=False, default='' ,size=255)
+      database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='img-id', required=False, array=False, default='', size=50)
+      database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='sender-id', required=True, array=False, default='',size=50)
+      database.create_string_attribute(database_id=dbId, collection_id=collection['$id'], key='session-id', required=True, array=False, default='',size=50)
+      print("Attributes created")
+      # Wait for 2 seconds to finishing creating the attributes
   except Exception as e:
     return res.json({
       "message": "Error creating collection or attributes",
