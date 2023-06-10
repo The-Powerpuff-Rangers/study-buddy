9e94e9028b1c4093c07316c92ed18f14cf4b64e2
Khushiyant <khushiyant2002@gmail.com>
2023-06-11 01:00:40 +0530

    chore: add token generation--.cjs
    Co-authored-by: Vidhu Mathur <Kranium2002@users.noreply.github.com>

--- src/utils/TkGenerator.cjs
+++ src/utils/TkGenerator.cjs
@@ -1,13 +1,14 @@
 const {RtcTokenBuilder, RtmTokenBuilder, RtcRole, RtmRole} = require('agora-token')
 
 const generateRtcToken = () => {
-
+  // Rtc Examples
   const appId = '6f098e3930d044e1b87449f38dc57ccf';
   const appCertificate = 'e94b5360356f4f89a3a17f2a0e5f0fb9';
-  var channelName = 'studybuddy';
-  var uid = 0;
+  const channelName = '1';
+  const uid=0;
+  const role = RtcRole.PUBLISHER;
 
-  const expirationTimeInSeconds = 3600
+  const expirationTimeInSeconds = 3600*24*30
 
   const currentTimestamp = Math.floor(Date.now() / 1000)
 
@@ -16,8 +17,12 @@ const generateRtcToken = () => {
   // IMPORTANT! Build token with either the uid or with the user account. Comment out the option you do not want to use below.
 
   // Build token with uid
-  const tokenA = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, privilegeExpiredTs);
-  return tokenA;
-
+  const tokenA = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs);
+  return tokenA
 }
+
 console.log(generateRtcToken());
+
+module.exports = {
+  generateRtcToken
+}
