8298ba464b6492dcc6f1bc98defcc40e3e711454
Khushiyant <khushiyant2002@gmail.com>
2023-06-11 23:44:54 +0530

    chore(add): chatroom--component

---
+++ src/components/ChatBoxControl.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let user;    // Value of the user input
+    export let send;    // Function that sends the current message
+    export let message; // Value of the message input
+  </script>
+  
+  <div class="controls p-2">
+    <!-- <p class="text-xs">Username: <input class="w-full rounded-md" placeholder="Type your username here..." bind:value={user} type="text" /></p> -->
+    
+    <textarea class="w-full h-20 mt-2 rounded-md resize-none" placeholder="Type your message here..." bind:value={message}></textarea>
+    <button class="w-full h-8 mt-2 bg-orange-500 text-white rounded-md transition duration-200 hover:bg-orange-400" on:click="{()=>send()}">Send</button>
+  </div>
+  
---
+++ src/components/ChatBubble.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let message; // Message object that contains all information about this specific message
+    export let user; // Username of the user that is currently using the chatbox
+  </script>
+  
+  <p class="{message.user == user ? 'text-right' : 'text-left'} text-xs text-gray-500">
+    {message.user} | {message.timestamp.toLocaleString()}
+  </p>
+  
+  <div class="{message.user == user ? 'flex justify-end' : 'flex justify-start'} items-center mt-2">
+    <div
+      class="w-80 px-2 py-1 rounded-lg {message.user == user ? 'bg-blue-500' : 'bg-gray-200'}"
+    >
+      <p class="text-xs">{message.message}</p>
+    </div>
+  </div>
+  
