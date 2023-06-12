d393bfae2b4758afbd7093401004e2dd53044421
Khushiyant <khushiyant2002@gmail.com>
2023-06-12 22:48:12 +0530

    chore(add): about page skelton

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -4,8 +4,11 @@
 </script>
 
 <Nav />
-<section class="h-screen w-full bg-neutral-300">
-  
+<section class="h-screen w-screen bg-neutral-300">
+  <div class="flex flex-row items-center justify-between h-full">
+    <h1 class="text-8xl text-white p-15 font-bold">Learn about Us</h1>
+    <img src="src/assets/illustrations/build-up.svg" class="h-2/3" alt="Colab"/>
+  </div>
 </section>
 
 <Footer />
