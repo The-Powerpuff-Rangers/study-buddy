0e088860a3b1f76037a56441915938c6308dcc2f
Khushiyant <khushiyant2002@gmail.com>
2023-06-12 23:52:30 +0530

    chore(add): meet toolbar

---
+++ src/components/MeetTool.svelte
@@ -0,0 +1,11 @@
+<div class="flex flex-row justify-center space-x-8 py-5">
+  <button id="mic" class="btn btn-primary px-5 py-5 rounded-full bg-white">
+    <img src="/src/assets/icons/mic.svg" alt="mic" />
+  </button>
+  <button id="leave" class="btn bg-primary-200 px-7 py-5 rounded-lg text-white"
+    >End Meeting</button
+  >
+  <button id="video" class="btn btn-primary px-5 py-5 rounded-full bg-white">
+    <img src="/src/assets/icons/camera.svg" alt="video" />
+  </button>
+</div>
