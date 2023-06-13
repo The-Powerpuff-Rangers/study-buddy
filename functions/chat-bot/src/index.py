53daa6cfa840fbe13aa65e2868ae55ca7b20049c
Khushiyant <khushiyant2002@gmail.com>
2023-06-14 00:37:10 +0530

    chore(add): rtc-generate function
    Co-authored-by: Vidhu Mathur <Kranium2002@users.noreply.github.com>

--- functions/chat-bot/src/index.py
+++ functions/chat-bot/src/index.py
@@ -7,7 +7,11 @@ def main(req, res):
     
 
     try:
-        message = json.load(str(req.payload))['message']
+        # message = json.load(str(req.payload))['message']
+
+        res.json({
+            "message": req.payload
+        })
 
         api_key = req.variables.get('OPENAI_API_KEY')
         bot = ChatBot(model="gpt-3.5-turbo-0301", api_key=api_key)
--- functions/rtc-generate/src/index.js
+++ functions/rtc-generate/src/index.js
@@ -1,15 +1,43 @@
-import { generateRtcToken } from "./TokenGenerator.js";
+var agora_token_1 = require("agora-token");
+
+function generateRtcToken(channelName, uid) {
+  var appId = "6f098e3930d044e1b87449f38dc57ccf";
+  var appCertificate = "e94b5360356f4f89a3a17f2a0e5f0fb9";
+  var expirationTimeInSeconds = 3600;
+  var currentTimestamp = Math.floor(Date.now() / 1000);
+  var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
+  // IMPORTANT! Build token with either the uid or with the user account. Comment out the option you do not want to use below.
+  // Build token with uid
+  var tokenA = agora_token_1.RtcTokenBuilder.buildTokenWithUid(
+    appId,
+    appCertificate,
+    channelName,
+    uid,
+    agora_token_1.RtcRole.PUBLISHER,
+    expirationTimeInSeconds,
+    privilegeExpiredTs
+  );
+  return tokenA;
+}
 
 module.exports = async function (req, res) {
   try {
-    let decodedJson = JSON.parse(req.body);
+    let decodedJson = JSON.parse(req.payload);
+    // return res.json({
+    //   decodedJson: req.body,
+    // });
 
     return res.json({
-      "token": generateRtcToken(decodedJson.channelName, decodedJson.uid),
+      token: generateRtcToken(decodedJson["channelName"], decodedJson["uid"]),
     });
   } catch (error) {
     return res.json({
-      "error": error.message,
+      error: error.message,
     });
   }
 };
+
+// {
+//   "channelName": "1",
+//   "uid": "1"
+// }
